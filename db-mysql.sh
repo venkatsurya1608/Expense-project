aa40ec893426e1b4c3c76b0206fef62e57c30a7f
venkatsurya <venkatsurya1608@gmail.com>
2024-04-25 15:36:46 +0530

    expense

--- db-mysql.sh
+++ db-mysql.sh
@@ -45,10 +45,3 @@ VALIDATE $? "Starting Mysql server"
 #Below code will be useful for idempotent nature
 mysql -h db.venkatdevops1608.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 
-if [ $? -ne 0 ]
-then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
-else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
-fi 
